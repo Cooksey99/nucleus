33cd43fe428ed0aad307eb328e7940a5793c4ee9
Cooksey99 <andrewcooksey42@gmail.com>
2026-01-27 19:29:35 -0600

    WIP: metrics layout began

---
+++ nucleus-core/src/metrics/collector.rs
@@ -0,0 +1,13 @@
+use crate::metrics::types::{MetricsSnapshot, ResourceUsage};
+use std::time::{Duration, Instant};
+
+pub trait MetricsCollector: Send + Sync {
+    fn collect(&self) -> anyhow::Result<ResourceUsage>;
+    
+    fn create_snapshot(&self, start: Instant) -> anyhow::Result<MetricsSnapshot> {
+        Ok(MetricsSnapshot {
+            timestamp: start.elapsed(),
+            resource_usage: self.collect()?,
+        })
+    }
+}
